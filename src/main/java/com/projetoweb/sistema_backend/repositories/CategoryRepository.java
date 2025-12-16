21b8c0e13b8af6b6f431c2f628f18d1c913ee94b
Geberson Antonio Ferreira Mendes <gebersonf@kyros.com.br>
2025-12-15 23:55:01 -0300

    implement category project

---
+++ src/main/java/com/projetoweb/sistema_backend/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.projetoweb.sistema_backend.repositories;
+
+import com.projetoweb.sistema_backend.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
