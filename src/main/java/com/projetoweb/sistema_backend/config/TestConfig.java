302f7bf2c4b706c3395766cfaab7a4c23e460948
Geberson Antonio Ferreira Mendes <gebersonf@kyros.com.br>
2025-12-16 23:49:18 -0300

    adding product in project

--- src/main/java/com/projetoweb/sistema_backend/config/TestConfig.java
+++ src/main/java/com/projetoweb/sistema_backend/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.projetoweb.sistema_backend.config;
 
 import com.projetoweb.sistema_backend.entities.Category;
 import com.projetoweb.sistema_backend.entities.Order;
+import com.projetoweb.sistema_backend.entities.Product;
 import com.projetoweb.sistema_backend.entities.User;
 import com.projetoweb.sistema_backend.entities.enums.OrderStatus;
 import com.projetoweb.sistema_backend.repositories.CategoryRepository;
 import com.projetoweb.sistema_backend.repositories.OrderRepository;
+import com.projetoweb.sistema_backend.repositories.ProductRepository;
 import com.projetoweb.sistema_backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -28,13 +30,23 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
